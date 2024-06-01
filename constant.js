1be4b95251b08be76e07232b72b7a68153e7c747
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-06-01 18:21:24 +0530

    Audio feature added

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
 const api_key = '7264336649:AAFQfjmTQ1tiug6hvIcaSX0wOsQ6FldAJ_w';
-module.exports = {
+module.exports ={
     apiKey: api_key
 }
--- index.js
+++ index.js
@@ -44,4 +44,49 @@ bot.on('text', async (ctx) => {
 });
 
 
+// Set-uping for the audio feature
+bot.command('audio', (ctx) => {
+    const url = ctx.message.text.split(' ')[1];
+    if (ytdl.validateURL(url)) {
+        ctx.reply('Downloading your audio...');
+        downloadAudio(ctx, url);
+    } else {
+        ctx.reply('Invalid URL. Please send a valid YouTube link.');
+    }
+});
+
+async function downloadAudio(ctx, url) {
+    try {
+        const info = await ytdl.getInfo(url);
+        const title = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+        const videoPath = path.resolve(__dirname, 'downloads', `${title}.mp4`);
+        const audioPath = path.resolve(__dirname, 'downloads', `${title}.mp3`);
+
+        ytdl(url, { quality: 'highestaudio' })
+            .pipe(fs.createWriteStream(videoPath))
+            .on('finish', () => {
+                ffmpeg(videoPath)
+                    .format('mp3')
+                    .on('end', async () => {
+                        await ctx.replyWithAudio({ source: audioPath });
+                        fs.unlinkSync(videoPath);
+                        fs.unlinkSync(audioPath);
+                    })
+                    .on('error', async (err) => {
+                        console.error(err);
+                        await ctx.reply('An error occurred while converting the audio.');
+                        fs.unlinkSync(videoPath);
+                    })
+                    .save(audioPath);
+            })
+            .on('error', async (err) => {
+                console.error(err);
+                await ctx.reply('An error occurred while downloading the audio.');
+            });
+    } catch (error) {
+        console.error(error);
+        ctx.reply('An error occurred while processing the audio.');
+    }
+}
+
 bot.launch()
